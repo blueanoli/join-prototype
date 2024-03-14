236851d4e40153b497389d0a405f05d535013ce2
ILONA KOSCHI <ILONA.KOSCHI@HOTMAIL.com>
2024-03-14 17:41:25 +0100

    added storage-token and set/get-item functions in script.js/add-task htmlcss templates wip

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const STORAGE_TOKEN = 'SUAP8YLQYG530FE8HDED8CKFONZBVXBSJ39FDPIR';
+const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+
+
+async function setItem(key, value) {
+    const payload = { key, value, token: STORAGE_TOKEN };
+    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
+    .then(res => res.json());
+}
+
+async function getItem(key) {
+    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(url).then(res => res.json());
+}

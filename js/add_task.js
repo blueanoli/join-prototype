3e909c640fc21f56f5222f96f6ef8878cd667835
ILONA KOSCHI <ILONA.KOSCHI@HOTMAIL.com>
2024-03-17 12:24:34 +0100

    changed select contacts html and css added basic rendercontacts and assigncontacts function work in progress

--- js/add_task.js
+++ js/add_task.js
@@ -1,9 +1,17 @@
 let subtaskCounter = 0;
+let contacts = ['Max Mustermann', 'Susi Sonne'];
 
+/**
+ * Initializes the script by including HTML content dynamically.
+ */
 function init() {
     includeHTML();
 }
 
+/**
+ * Dynamically includes HTML content into elements with the 'w3-include-html' attribute.
+ * The function fetches the HTML file specified in the attribute and embeds its content into the element.
+ */
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
@@ -18,6 +26,10 @@ async function includeHTML() {
     }
 }
 
+/**
+ * Sets the task priority to urgent by updating the UI elements' class and image source accordingly.
+ * It also removes the medium and low priority indicators if they are active.
+ */
 function chooseUrgentPrio() {
     let urgent = document.getElementById("priority-urgent");
     let urgentimg = document.getElementById("img-urgent");
@@ -29,6 +41,10 @@ function chooseUrgentPrio() {
     removeLowPrio();
 }
 
+/**
+ * Sets the task priority to medium by updating the UI elements' class and image source accordingly.
+ * It also removes the low and urgent priority indicators if they are active.
+ */
 function chooseMediumPrio() {
     let medium = document.getElementById("priority-medium");
     let mediumimg = document.getElementById("img-medium");
@@ -40,6 +56,10 @@ function chooseMediumPrio() {
     removeUrgentPrio();
 }
 
+/**
+ * Sets the task priority to low by updating the UI elements' class and image source accordingly.
+ * It also removes the medium and urgent priority indicators if they are active.
+ */
 function chooseLowPrio() {
     let low = document.getElementById("priority-low");
     let lowimg = document.getElementById("img-low");
@@ -51,6 +71,9 @@ function chooseLowPrio() {
     removeUrgentPrio();
 }
 
+/**
+ * Clears the task form, resets priority indicators and disables the add task button.
+ */
 function clearForm() {
     document.getElementById('add-task').reset();
     document.getElementById("add-task-btn").disabled = true;
@@ -60,9 +83,15 @@ function clearForm() {
     removeLowPrio();
 }
 
+/**
+ * Adds a new task and displays a notification for a brief period before navigating to the board page.
+ */
 function addTask() {
     let notification = document.getElementById('notification-container');
 
+    if (notification.innerHTML !== '') {
+        return;
+    }else{
     notification.classList.add("animate");
 
     notification.innerHTML = /*html*/ `
@@ -79,7 +108,12 @@ function addTask() {
         window.location.href = "board.html";
     }, 1000);
 }
+}
 
+/**
+ * Adds a subtask to the task form.
+ * Each subtask is assigned a unique ID and can be edited or removed.
+ */
 function addSubtask(){
     let subtask = document.getElementById('subtasks').value;
     let subtaskcontainer = document.getElementById('subtask-container');
@@ -101,11 +135,48 @@ function addSubtask(){
     document.getElementById('subtasks').value = '';
 }
 
+/**
+ * Removes a subtask from the task form based on its unique ID.
+ * @param {string} subtaskId - The ID of the subtask to be removed.
+ */
 function removeSubtask(subtaskId){
     let subtaskElement = document.getElementById(subtaskId);
     if(subtaskElement) {
         subtaskElement.remove(); 
     }
+}
+
+/**
+ * Populates the assigned-to select element with contact names.
+ * Each contact is assigned a unique ID and can be edited or removed.
+ * 
+ */
+function renderContacts() {
+    let dropdown = document.getElementById('assigned-to');
+    let itemsDiv = dropdown.querySelector('.select-items');
+    let selectedDiv = dropdown.querySelector('.select-selected');
+
+    itemsDiv.innerHTML = '';
+
+    contacts.forEach(contact => {
+        let optionDiv = document.createElement(`div`);
+        optionDiv.innerHTML = /*html*/ `
+        ${contact} <img id="img-checkbox" onclick="assignContact('${contact}')" src="assets/img/checkboxempty.svg">
+        `;
+        optionDiv.addEventListener('click', function() {
+            selectedDiv.innerHTML = `${contact}`;
+            itemsDiv.classList.add('select-hide');
+            changeDropdownImg('close');
+        });
+        itemsDiv.appendChild(optionDiv);
+    });
+}
+
+function assignContact(contact) {
+    let assignedTo = document.getElementById('assign-contacts');
+    assignedTo.innerHTML += /*html*/ `
+    <div class="assigned-contact">${contact}</div>
+    `;
 }
 
 // HILFSFUNKTIONEN
@@ -129,3 +200,18 @@ function removeUrgentPrio(){
     urgent.classList.remove("urgent-priority-active");
     urgentimg.src = "assets/img/addtask_urgent.svg";
 }
+
+function toggleDropdown() {
+    let itemsDiv = document.getElementById('assigned-to').querySelector('.select-items');
+    itemsDiv.classList.toggle('select-hide');
+    changeDropdownImg(itemsDiv.classList.contains('select-hide') ? 'close' : 'open');
+}
+
+function changeDropdownImg(state) {
+    let img = document.getElementById("img-dropdown");
+    img.src = state === 'open' ? "assets/img/addtask_dropdown_up.svg" : "assets/img/addtask_dropdown.svg";
+}
+
+document.getElementById('assigned-to').addEventListener('click', function(e) {
+    e.stopPropagation();
+});

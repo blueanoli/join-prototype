ddde26d94e69e06cd5bc58af64d7f8a73bb1e830
philschoeferle <philipp.schoeferle@web.de>
2024-03-18 12:27:51 +0100

    animation js prototype

---
+++ js/login.js
@@ -0,0 +1,23 @@
+/*function initLogin() {
+    statusVisit();
+}
+
+function statusVisit() {
+  let isFirstVisit = localStorage.getItem("isFirstVisit");
+  if (!isFirstVisit) {
+    localStorage.setItem("isFirstVisit", "true");
+    playAnimation();
+  }
+}
+
+function playAnimation() {
+    let animatedLogoContainer = document.querySelector('.animated-logo-container');
+    let animatedLogo = document.getElementById('animated-logo');
+    if (animatedLogoContainer && animatedLogo) {
+      animatedLogoContainer.style.display = 'block';
+      animatedLogo.style.display = 'block';
+      animatedLogoContainer.offsetWidth;
+      animatedLogoContainer.classList.add('animate');
+      animatedLogo.classList.add('animate');
+    }
+  } */

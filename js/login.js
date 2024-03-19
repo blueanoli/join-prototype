ab112228a8bb34b19887882f9f9a19fdea2f67db
philschoeferle <philipp.schoeferle@web.de>
2024-03-19 09:36:55 +0100

    animated logo centered and function to execute only once per session

--- js/login.js
+++ js/login.js
@@ -1,23 +1,27 @@
-/*function initLogin() {
-    statusVisit();
+function initLogin() {
+  statusVisit();
 }
 
+/* Used to check if it's the person first visit in the current session 
+and sets its value to false, so it won't execute repeatedly */
 function statusVisit() {
-  let isFirstVisit = localStorage.getItem("isFirstVisit");
-  if (!isFirstVisit) {
-    localStorage.setItem("isFirstVisit", "true");
-    playAnimation();
+  let isFirstVisit = sessionStorage.getItem("isFirstVisit");
+  if (isFirstVisit === null || isFirstVisit === "true") {
+    playLogoAnimation();
+    sessionStorage.setItem("isFirstVisit", "false");
   }
 }
 
-function playAnimation() {
-    let animatedLogoContainer = document.querySelector('.animated-logo-container');
-    let animatedLogo = document.getElementById('animated-logo');
-    if (animatedLogoContainer && animatedLogo) {
-      animatedLogoContainer.style.display = 'block';
-      animatedLogo.style.display = 'block';
-      animatedLogoContainer.offsetWidth;
-      animatedLogoContainer.classList.add('animate');
-      animatedLogo.classList.add('animate');
-    }
-  } */
+/* Executes the animated Logo if it's the first visit in the current session */
+function playLogoAnimation() {
+  let animatedLogoContainer = document.querySelector(
+    ".animated-logo-container"
+  );
+  let animatedLogo = document.getElementById("animated-logo");
+
+  if (animatedLogoContainer && animatedLogo) {
+    animatedLogoContainer.style.display = "block";
+    animatedLogoContainer.classList.add("animate");
+    animatedLogo.classList.add("animate");
+  }
+}

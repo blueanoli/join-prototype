96ad661a5d443ce37080b485e46bbed4db48e145
ILONA KOSCHI <ILONA.KOSCHI@HOTMAIL.com>
2024-03-22 07:41:20 +0100

    fixed bug where clearForm would deactivate error-messages and addtask btn

--- js/add_task.js
+++ js/add_task.js
@@ -276,7 +276,7 @@ function renderCategories() {
 
 function clearForm() {
     document.getElementById('add-task').reset();
-    document.getElementById("add-task-btn").disabled = true;
+    document.getElementById("add-task-btn").disabled = false;
 
     removeLowPrio();
     removeUrgentPrio();

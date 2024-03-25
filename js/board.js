1e54c6013595d8cde3c88749bd706db88e5904f6
ILONA KOSCHI <ILONA.KOSCHI@HOTMAIL.com>
2024-03-25 15:48:06 +0100

    added openAddTask function and started with overlay css

--- js/board.js
+++ js/board.js
@@ -1,3 +1,12 @@
 async function renderBoard(){
     await init();
 }
+
+function openAddTask() {
+    const container = document.getElementById('add-task-container-board');
+    container.setAttribute('w3-include-html', 'assets/templates/task-form.html');
+
+    includeHTML().then(() => {
+        container.style.display = 'block';
+    });
+}

a3fe09dd59a36fccaf2632ed788ba3ac85c51bb5
ILONA KOSCHI <ILONA.KOSCHI@HOTMAIL.com>
2024-03-26 09:27:45 +0100

    added function to check if columns are empty to display text

--- js/board.js
+++ js/board.js
@@ -1,5 +1,30 @@
+let sections = [
+    { id: 'board-todo-container', text: 'No tasks to do' },
+    { id: 'board-in-progress-container', text: 'No tasks in progress' },
+    { id: 'board-feedback-container', text: 'No tasks await feedback' },
+    { id: 'board-done-container', text: 'No tasks done' }
+];
+
 async function renderBoard(){
     await init();
+    checkAllSections();
+}
+
+function checkAllSections() {
+    for (let i = 0; i < sections.length; i++) {
+        checkColumnEmpty(sections[i].id, sections[i].text);
+    }
+}
+
+function checkColumnEmpty(sectionId, emptyText) {
+    let section = document.getElementById(sectionId);
+
+    if (!section.hasChildNodes()) {
+        section.innerHTML = /*html*/`
+        <div class='empty-column'>
+        <span>${emptyText}</span>
+        </div>`;
+    }
 }
 
 function openAddTask() {
@@ -22,11 +47,9 @@ function openAddTask() {
 function closeAddTask() {
     const container = document.getElementById('add-task-container-board');
     const overlay = document.getElementById('page-overlay');
-    const close = document.getElementById('close-task-btn');
     const body = document.body;
 
     container.classList.remove('active'); 
-    close.classList.remove('active');
     overlay.style.display = 'none'; 
     body.style.overflow = ''; 
 }

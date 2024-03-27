abba0b63d3bd1e0f72d29acb64529b3a16038f9f
philschoeferle <philipp.schoeferle@web.de>
2024-03-27 18:05:45 +0100

    contacts function

--- js/contacts.js
+++ js/contacts.js
@@ -1,44 +1,98 @@
-let testContacts = [
+let contacts = [
   {
     name: "Tatjana Wolf",
     email: "wolf@gmail.de",
   },
+  {
+    name: "Peter Lustig",
+    email: "lustig@gmail.de",
+  },
+  {
+    name: "Philipp Plein",
+    email: "plein@gmail.de",
+  },
+  {
+    name: "Dan Schneider",
+    email: "schneider@gmail.de",
+  },
+  {
+    name: "Xavier Klein",
+    email: "klein@gmail.de",
+  },
 ];
 
+let contactsByLetter = [];
+
 function initContacts() {
-  logOut();
+  isNotLoggedIn();
   displayLetterContacts();
 }
 
-function logOut() {
+function isNotLoggedIn() {
   if (sessionStorage.getItem("isLoggedIn") !== "true") {
     window.location.href = "index.html";
   }
 }
 
 function displayLetterContacts() {
+  for (let i = 0; i < contacts.length; i++) {
+    let contact = contacts[i];
+    let firstLetter = contact["name"].charAt(0).toUpperCase();
+
+    if (!contactsByLetter[firstLetter]) {
+      contactsByLetter[firstLetter] = [];
+    }
+    contactsByLetter[firstLetter].push(contact);
+  }
+  showContacts();
+}
+
+function showContacts() {
+  let letters = Object.keys(contactsByLetter);
+
   let main = document.getElementById("contacts-main");
   main.innerHTML = "";
 
-  for (let i = 0; i < testContacts.length; i++) {
-    let contactsName = testContacts[i]["name"];
-    let contactsEmail = testContacts[i]["email"];
-    let firstLetter = contactsName.charAt(0).toUpperCase();
-    let firstLetters = contactsName.match(/\b(\w)/g);
-    let acronym = firstLetters.join("").toUpperCase();
+  for (let i = 0; i < letters.length; i++) {
+    let letter = letters[i];
 
-    main.innerHTML += `
-    <div class="contacts-letter-container">
-        <span>${firstLetter}</span>
-    </div>
-    <div class="contacts-letter-border"></div>
-    <div class="contacts-contact-data">
-      <div> ${acronym}</div>
-      <div class="contacts-contact-details">
-        <span>${contactsName}</span>
-        <span>${contactsEmail}</span>
-      </div>
-    </div>
-    `;
+    letterContainerHTML(main, letter);
+    let contacts = contactsByLetter[letter];
+
+    for (let j = 0; j < contacts.length; j++) {
+      let contact = contacts[j];
+
+      contactContainerHTML(main, contact);
+    }
   }
+}
+
+function letterContainerHTML(main, letter) {
+  return (main.innerHTML += `
+  <div class="contacts-letter-container">
+    <span>${letter}</span>
+  </div>
+  <div class="contacts-letter-border"></div>`);
+}
+
+function contactContainerHTML(main, contact) {
+  let contactsName = contact["name"];
+  let contactsEmail = contact["email"];
+
+  return (main.innerHTML += `
+  <div class="contacts-contact-data">
+  <div>${getAcronyms(contactsName)}</div>
+  <div class="contacts-contact-details">
+    <span>${contactsName}</span>
+    <span>${contactsEmail}</span>
+  </div>
+</div>
+`);
+}
+
+function getAcronyms(contactsName) {
+  let firstLetters = contactsName.match(/\b(\w)/g);
+  let acronym = firstLetters.join("").toUpperCase();
+
+  return acronym;
 }

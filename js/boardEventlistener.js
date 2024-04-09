fc16e9b7f251d9bccbe5f08a45e70d89897f830c
ILONA KOSCHI <ILONA.KOSCHI@HOTMAIL.com>
2024-04-09 13:34:12 +0200

    added jsdoc to boardEventlistenerJS

--- js/boardEventlistener.js
+++ js/boardEventlistener.js
@@ -1,11 +1,16 @@
 let autoScrollInterval;
-// DRAG AND DROP LOGIC ---------------------------------------------------------------------------------------------------------
+
+/** Initializes hover effects for all mini task containers. */
 function initializeHoverEffect() {
     document.querySelectorAll('.mini-task-container').forEach(minitask => {
         initializeMinitaskHoverEvents(minitask);
     });
 }
 
+/**
+ * Initializes hover events for a mini task.
+ * @param {Element} minitask - Mini task element.
+ */
 function initializeMinitaskHoverEvents(minitask) {
     if (isLargeScreen()) {
         setHoverEvents(minitask);
@@ -14,6 +19,10 @@ function initializeMinitaskHoverEvents(minitask) {
     }
 }
 
+/**
+ * Sets hover events for a mini task.
+ * @param {Element} minitask - Mini task element.
+ */
 function setHoverEvents(minitask) {
     let hoverTimer;
 
@@ -23,31 +32,51 @@ function setHoverEvents(minitask) {
     minitask.addEventListener('mouseleave', () => handleMouseLeave(minitask, hoverTimer));
 }
 
+/**
+ * Handles mouse enter event for mini task.
+ * @param {Element} minitask - Mini task element.
+ * @param {number} hoverTimer - Hover timer.
+ */
 function handleMouseEnter(minitask, hoverTimer) {
     hoverTimer = setTimeout(() => {
         minitask.style.cursor = 'grab';
     }, 500);
 }
 
+/**
+ * Handles mouse down event for mini task.
+ * @param {Element} minitask - Mini task element.
+ */
 function handleMouseDown(minitask) {
     minitask.style.cursor = 'grabbing';
 }
 
+/**
+ * Handles mouse up event for mini task.
+ * @param {Element} minitask - Mini task element.
+ */
 function handleMouseUp(minitask) {
     minitask.style.cursor = 'grab';
 }
 
+/**
+ * Handles mouse leave event for mini task.
+ * @param {Element} minitask - Mini task element.
+ * @param {number} hoverTimer - Hover timer.
+ */
 function handleMouseLeave(minitask, hoverTimer) {
     clearTimeout(hoverTimer);
     minitask.style.cursor = 'pointer';
 }
 
+/** Initializes drag and drop events.*/
 function initializeDragAndDrop() {
     initializeTasksDragEvents();
     initializeColumnsDragEvents();
     initializeHoverEffect();
 }
 
+/** Initializes drag events for all mini task containers */
 function initializeTasksDragEvents() {
     document.querySelectorAll('.mini-task-container').forEach(item => {
         item.addEventListener('dragstart', handleDragStartEvent);
@@ -55,12 +84,19 @@ function initializeTasksDragEvents() {
     });
 }
 
+/**
+ * Initializes drag events for all progress columns.
+ */
 function initializeColumnsDragEvents() {
     document.querySelectorAll('.progress-column').forEach(column => {
         initializeColumnDragEvents(column);
     });
 }
 
+/**
+ * Initializes drag events for column.
+ * @param {Element} column - Column element.
+ */
 function initializeColumnDragEvents(column) {
     const dropZone = column.querySelector('.dotted-container-drag-drop');
 
@@ -69,18 +105,30 @@ function initializeColumnDragEvents(column) {
     column.addEventListener('drop', handleDropEvent);
 }
 
+/**
+ * Handles drag start event.
+ * @param {Event} event - Drag start event.
+ */
 function handleDragStartEvent(event) {
     if (isLargeScreen()) {
         handleDragStart(event);
     }
 }
 
+/**
+ * Handles drag end event.
+ * @param {Event} event - Drag start event.
+ */
 function handleDragEndEvent(event) {
     if (isLargeScreen()) {
         handleDragEnd(event);
     }
 }
 
+/**
+ * Handles drag over event.
+ * @param {Element} dropZone - Drop zone element.
+ */
 function handleDragOverEvent(event, dropZone) {
     if (isLargeScreen()) {
         event.preventDefault();
@@ -88,32 +136,47 @@ function handleDragOverEvent(event, dropZone) {
     }
 }
 
+/**
+ * Handles drag leave event.
+ * @param {Element} column - Column element.
+ */
 function handleDragLeaveEvent(event, column, dropZone) {
     if (isLargeScreen() && !column.contains(event.relatedTarget)) {
         dropZone.style.display = 'none';
     }
 }
 
+/** Handles drop event. */
 function handleDropEvent(event) {
     if (isLargeScreen()) {
         handleDrop(event);
     }
 }
 
+/**
+ * Checks if screen size is large.
+ * @returns {boolean} - True if screen width is greater than 1240, else false.
+ */
 function isLargeScreen() {
     return window.innerWidth > 1240;
 }
 
+/** Handles window resize event by reinitializing drag and drop functionality.*/
 function handleResize() {
     initializeDragAndDrop();
 }
 
+/**
+ * Handles drag start event.
+ * @param {Event} event - Drag start event.
+ */
 function handleDragStart(event) {
     event.dataTransfer.setData('text/plain', event.target.dataset.taskId);
     event.target.classList.add('dragging');
     document.addEventListener('mousemove', handleMouseMove);
 }
 
+/** Adds 'dragstart' and 'dragend' event listeners to all mini task containers. */
 document.querySelectorAll('.mini-task-container').forEach(item => {
     item.addEventListener('dragstart', handleDragStart);
     item.addEventListener('dragend', function (e) {
@@ -121,37 +184,72 @@ document.querySelectorAll('.mini-task-container').forEach(item => {
     });
 });
 
+/**
+ * Handles drag over event. */
 function handleDragOver(event) {
     event.preventDefault();
 }
 
+/**
+ * Handles drop event. */
 function handleDrop(event) {
     event.preventDefault();
     let taskIndex = event.dataTransfer.getData('text/plain');
-    let taskElement = document.querySelector(`.mini-task-container[data-task-id="${taskIndex}"]`);
-    let targetContainer;
+    let taskElement = getTaskElement(taskIndex);
+    let targetContainer = getTargetContainer(event);
 
-    if (event.target.classList.contains('dotted-container-drag-drop')) {
-        targetContainer = event.target.previousElementSibling;
-    } else {
-        targetContainer = event.target.closest('.dotted-container');
-    }
-
-    if (!targetContainer) {
+    if (!targetContainer || !taskElement) {
         return; 
     }
 
-    if (taskElement) {
-        targetContainer.appendChild(taskElement);
-        let placeholder = targetContainer.querySelector('.empty-column');
-        if (placeholder) {
-            placeholder.style.display = 'none';
-        }
-        updateTaskStatus(parseInt(taskIndex, 10), targetContainer.id);
-        checkAllSections();
+    moveTaskToContainer(taskElement, targetContainer);
+    hidePlaceholderIfPresent(targetContainer);
+    updateTaskStatus(parseInt(taskIndex, 10), targetContainer.id);
+    checkAllSections();
+}
+
+/**
+ * Retrieves task element with specified index.
+ * @param {number} taskIndex - Index of task.
+ * @returns {Element} - Task element.
+ */
+function getTaskElement(taskIndex) {
+    return document.querySelector(`.mini-task-container[data-task-id="${taskIndex}"]`);
+}
+
+/**
+ * Retrieves target container from event.
+ * @returns {Element} - Target container element.
+ */
+function getTargetContainer(event) {
+    if (event.target.classList.contains('dotted-container-drag-drop')) {
+        return event.target.previousElementSibling;
+    } else {
+        return event.target.closest('.dotted-container');
     }
 }
 
+/**
+ * Moves task element to target container.
+ * @param {Element} taskElement - Task element.
+ * @param {Element} targetContainer - Target container.
+ */
+function moveTaskToContainer(taskElement, targetContainer) {
+    targetContainer.appendChild(taskElement);
+}
+
+/**
+ * Hides placeholder if present in target container.
+ * @param {Element} targetContainer - Target container.
+ */
+function hidePlaceholderIfPresent(targetContainer) {
+    let placeholder = targetContainer.querySelector('.empty-column');
+    if (placeholder) {
+        placeholder.style.display = 'none';
+    }
+}
+
+/** Handles drag end event. */
 function handleDragEnd(event) {
     event.target.classList.remove('dragging');
     document.querySelectorAll('.dotted-container-drag-drop').forEach(dropZone => {
@@ -164,6 +262,11 @@ function handleDragEnd(event) {
     document.removeEventListener('mousemove', handleMouseMove);
 }
 
+/**
+ * Updates status of task.
+ * @param {number} taskIndex - Index of task.
+ * @param {string} newColumnId - ID of new column.
+ */
 async function updateTaskStatus(taskIndex, newColumnId) {
     if (taskIndex < 0 || taskIndex >= tasksData.length) {
         return;
@@ -176,6 +279,7 @@ async function updateTaskStatus(taskIndex, newColumnId) {
     checkAllSections();
 }
 
+/** Handles mouse move event */
 function handleMouseMove(event) {
     let scrollSpeed = 1;
     let scrollMargin = 200;
@@ -197,6 +301,7 @@ function handleMouseMove(event) {
     }
 }
 
+/** Changes color of button on mouseover and mouseout events. */
 function changeButtonColor() {
     let btnMobile = document.getElementById('add-task-btn-board-mobile');
 
@@ -211,7 +316,7 @@ function changeButtonColor() {
     }
 }
 
-//EDIT TASK EVENTLISTENER ---------------------------------------------------------------------------------------------------------
+/** Sets up event listeners for edit form. */
 function setupEditFormEventListeners() {
     let editForm = document.querySelector('.edit-task-container');
     if (editForm) {
@@ -224,6 +329,7 @@ function setupEditFormEventListeners() {
     }
 }
 
+/** Disables form submission on pressing the Enter key. */
 function disableEditFormEnterKeySubmission() {
     let form = document.getElementById('edit-task-container');
     if (form) {
@@ -236,6 +342,7 @@ function disableEditFormEnterKeySubmission() {
     }
 }
 
+/** Adds an event listener to the edit subtask input. */
 function addEditSubtaskEventListener() {
     let subtaskInput = document.getElementById('edit-subtasks');
     let iconContainer = document.getElementById('edit-icon-container');
@@ -249,6 +356,7 @@ function addEditSubtaskEventListener() {
     });
 }
 
+/**  Sets up an event listener for the edit input. */
 function setupEditInputEventListener() {
     let subtaskInput = document.getElementById('edit-subtasks');
     subtaskInput.addEventListener('keydown', function (event) {
@@ -266,6 +374,7 @@ function setupEditInputEventListener() {
     });
 }
 
+/** Adds event listeners to the board  */
 function addBoardEventListeners() {
     document.body.addEventListener('click', function (event) {
         if (event.target.matches('.edit-icon-task')) {
@@ -275,10 +384,12 @@ function addBoardEventListeners() {
     });
     handleResize();
     changeButtonColor();
-
     window.addEventListener('resize', handleResize);
 }
 
+/**
+ * Sets up event listeners for subtasks.
+ */
 function setupSubtaskEventListeners() {
     setupEditFormEventListeners();
     disableEditFormEnterKeySubmission();
